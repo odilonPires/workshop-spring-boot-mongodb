5385c7ac317b8cb3b09dfb1fcc27011d573cf60b
Odilon Pires <odilon_pires@hotmail.com>
2024-02-15 14:55:55 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/overbo/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.overbo.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.overbo.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
